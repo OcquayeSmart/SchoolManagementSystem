953f649c0d56e09156a88c7c90744f5740540bba
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-07-12 22:34:52 -0100

    feat(NonTeachingStaff): Added Jobtitle field to non-teaching staff class

--- src/NonTeachingStaff.java
+++ src/NonTeachingStaff.java
@@ -1,6 +1,10 @@
 public class NonTeachingStaff extends Staff{
-
+    String jobTitle;
     public NonTeachingStaff(int id, String firstName, String lastName, String dateOfBirth, String staffId, double salary, String role, String jobTitle) {
         super(id, firstName, lastName, dateOfBirth, staffId, salary, role);
+        this.jobTitle = jobTitle;
+    }
+    public String getJobTitle() {
+        return jobTitle;
     }
 }
